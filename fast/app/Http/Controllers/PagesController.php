897b6e6ab3f07b7a8aa3f92a9db1a9dc49596965
KulanS <kgks999@gmail.com>
2017-10-06 19:04:47 +0530

    Init

---
+++ fast/app/Http/Controllers/PagesController.php
@@ -0,0 +1,22 @@
+<?php
+    namespace App\Http\Controllers;
+
+    class PagesController extends Controller{
+        public function getIndex(){
+            return view('pages/welcome');
+        }
+        
+        public function getContact(){
+            return view('pages/contact');
+        }
+        
+        public function getAbout(){
+            $address = "Galle Rd, Colombo.";
+            $phone = "0112225222";
+            $email = "fastsrilanka@gmail.com";
+            
+            return view('pages/about')->withAddress($address)->withPhone($phone)->withEmail($email);
+        }
+    }
+        
+?>
---
+++ fast/resources/views/pages/welcome.blade.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+  </head>
+  <body>
+   
+    <!--Pills with drop down(navbar begins)-->
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">FAST Sri Lanka</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Packages</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/contact">Contact</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="/about">About</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Profile
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Log Out</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+<br>
+    <!--Pills with drop down(navbar ends)-->
+    
+   
+    <!--<h1>Hello, world!</h1>-->
+     <!--Basic idea of the page (jumbotron begins)-->
+      <div class="container">
+          <div class="row">
+              <div class ="col-md-12">
+                <div class="jumbotron">
+                  <h1 class="display-3">Welcome to FAST Sri Lanka</h1>
+                  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                  <hr class="my-4">
+                  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                  <p class="lead">
+                    <a class="btn btn-primary btn-lg" href="#" role="button">Find Out More</a>
+                  </p>
+                </div> <!--Basic idea of the page (jumbotron ends)-->
+              </div>
+          </div>
+          
+          <div class="row">
+              <div class="col-md-8">
+                  <div class="description">
+                      <h2>News</h2>
+                      <p>Improve Your Paper by Writing Structured Paragraphs ... J. Ward Moorehouse focuses on making business connections but never forms any relationships.</p>
+                      <a href="#" class="btn btn-primary">Read More</a>                        
+                  </div>
+              </div>
+              <hr>
+              <div class="col-md-3 col-md-offset-1">
+                  <h2>Announcements</h2>
+              </div>
+          </div>
+      </div>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+  </body>
+</html>
