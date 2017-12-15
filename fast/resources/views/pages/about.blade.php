1e819f5f2bc518ea3882c3a68ed4d49ff73baff2
KumudiniPerera <kkperera14@gmail.com>
2017-12-15 12:25:00 +0530

    test

--- fast/resources/views/pages/about.blade.php
+++ fast/resources/views/pages/about.blade.php
@@ -5,7 +5,7 @@
           <div class="row">
               <div class="col-md-12">
                   <h1>Our Mission</h1>
-                  <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                  <p>This  is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
                   <br>
                   <h1>Our Vision</h1>
                    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
